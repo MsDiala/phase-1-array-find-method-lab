b069bf90c021f4a4ebc22e12c18ee973d9305437
MsDiala <diala-is-diala@hotmail.com>
2023-10-26 18:51:09 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+  
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const record = [
+    { year: "2018", result: "L" },
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" },
+    { year: "2015", result: "W" },
+    { year: "2014", result: "L" },
+    { year: "2013", result: "W" },
+  ];
+  
+  const winYear = superbowlWin(record);
+  console.log("Superbowl Win Year:", winYear);
